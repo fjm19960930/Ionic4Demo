19f6c6926d70a0eb1625184a566e1c541acd1289
fanjiaming <2985474861@qq.com>
2019-06-26 21:01:17 +0800

    video示例修改

--- src/app/pages/video-player/video-player.page.ts
+++ src/app/pages/video-player/video-player.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {VgMedia} from '_videogular2@6.4.0@videogular2/core';
+import {VgMedia, BitrateOption} from '_videogular2@6.4.0@videogular2/core';
 
 @Component({
   selector: 'app-video-player',
@@ -9,9 +9,28 @@ import {VgMedia} from '_videogular2@6.4.0@videogular2/core';
 export class VideoPlayerPage implements OnInit {
 
     @ViewChild('myMedia') myMedia: VgMedia;
-      constructor() { }
+    videoPath:string;
+    videoQuality1: BitrateOption = {
+        qualityIndex: 720,
+        width: 500,
+        height: 300,
+        bitrate: 1,
+        mediaType: "video/mp4",
+        label: "高清",
+    };
+    videoQuality2: BitrateOption = {
+        qualityIndex: 960,
+        width: 500,
+        height: 300,
+        bitrate: 1,
+        mediaType: "video/mp4",
+        label: "超清",
+    };
+    dashBitrates:Array<BitrateOption> = [];
+    constructor() { }
 
-      ngOnInit() {
-      }
-
+    ngOnInit() {
+        this.videoPath = "assets/demo.mp4";
+        this.dashBitrates.push(this.videoQuality1, this.videoQuality2);
+    }
 }
